4a30852903abb61f212d8bb95784add864528254
Emilio Reyes <lerp2192@gmail.com>
2024-09-22 00:21:40 -0600

    form validation

--- src/app/api/openai.service.ts
+++ src/app/api/openai.service.ts
@@ -9,15 +9,9 @@ export default class AIService {
     private client: OpenAI;
 
     constructor() {
-        console.log({
-            organization: process.env.NEXT_PUBLIC_ORGANIZATION_OPENAI,
-            apiKey: process.env.NEXT_PUBLIC_API_KEY_OPENAI,
-            dangerouslyAllowBrowser: true 
-        });
         this.client = new OpenAI({
             organization: process.env.NEXT_PUBLIC_ORGANIZATION_OPENAI,
-            // apiKey: process.env.NEXT_PUBLIC_API_KEY_OPENAI,
-            apiKey: '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',
+            apiKey: process.env.NEXT_PUBLIC_API_KEY_OPENAI,
             dangerouslyAllowBrowser: true 
         });
     }
